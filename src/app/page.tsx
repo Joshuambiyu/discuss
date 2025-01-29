acb92237908b370fe218b74ea4df57bac8d83103
leVert <selmamathis15@gmail.com>
2025-01-29 17:42:01 +0300

    hard push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import TopicList from "@/components/topics/topic-list";
 import { Divider } from "@nextui-org/react";
 
 export default function Home() {
-  return (<div className="grid grid-cols-4 gap-4 p-4">
+  return (
+  <div className="grid grid-cols-4 gap-4 p-4">
     <div className="col-span-3">
       <h1 className="text-xl m-2">Top Posts</h1>
     </div>
